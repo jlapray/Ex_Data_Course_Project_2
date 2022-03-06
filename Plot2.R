6c5fc7e4da4c6be60ad73e04ff52a3ef82c7ca47
Jayce LaPray <100650985+jlapray@users.noreply.github.com>
2022-03-06 17:17:37 -0600

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,8 @@
+## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == “24510”) from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+
+baltimore <- subset(NEI, NEI$fips == "24510")
+
+totalBaltimore <- aggregate(Emissions ~ year, baltimore, sum)
+
+plot(totalBaltimore$year, totalBaltimore$Emissions, type = "o", main = expression("Total Baltimore" ~ PM[2.5] ~ "Emissions by Year"), xlab = "Year", ylab = expression("Total Baltimore "~ PM[2.5] ~ "Emissions"), col = "steelblue3")
+
